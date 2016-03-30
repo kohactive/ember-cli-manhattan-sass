708db50fbeb02bee7b3726ee0b3e0fd87d829e6e
Lawrence Davis <lawrence@kohactive.com>
2016-03-30 11:52:14 -0500

    Initial commit

---
+++ blueprints/ember-cli-manhattan-sass/index.js
@@ -0,0 +1,24 @@
+/*jshint node:true*/
+module.exports = {
+  description: '',
+
+  normalizeEntityName: function() {
+  },
+
+  afterInstall: function() {
+    return this.addBowerPackagesToProject([
+      {
+        name: 'bourbon',
+        target: '~> 4.2'
+      },
+      {
+        name: 'neat',
+        target: '^1.7.4'
+      },
+      {
+        name: 'manhattan',
+        target: '0.5.0'
+      }
+    ]);
+  }
+};
--- index.js
+++ index.js
@@ -1,6 +1,33 @@
+
 /* jshint node: true */
 'use strict';
 
+var path = require('path');
+var Funnel = require('broccoli-funnel');
+var MergeTrees = require('broccoli-merge-trees');
+
 module.exports = {
-  name: 'ember-cli-mahattan-sass'
+  name: 'ember-cli-manhattan-sass',
+
+  blueprintsPath: function() {
+    return path.join(__dirname, 'blueprints');
+  },
+
+  treeForStyles: function() {
+    var assetGemPaths = {
+      srcDir: '/assets/stylesheets',
+      destDir: '/app/styles'
+    };
+
+    var bourbonPath = path.join(this.project.bowerDirectory, 'bourbon', 'app');
+    var bourbonTree = new Funnel(this.treeGenerator(bourbonPath), assetGemPaths);
+
+    var neatPath = path.join(this.project.bowerDirectory, 'neat', 'app');
+    var neatTree = new Funnel(this.treeGenerator(neatPath), assetGemPaths);
+
+    var manhattanPath = path.join(this.project.bowerDirectory, 'manhattan', 'app');
+    var manhattanTree = new Funnel(this.treeGenerator(manhattanPath), assetGemPaths);
+
+    return new MergeTrees([bourbonTree, neatTree, manhattanTree]);
+  }
 };
